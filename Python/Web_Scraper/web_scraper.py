56dbc595b1b3767485c94facb6638974b4c520bd
Emmanuel <icetrack06@gmail.com>
2024-04-19 13:29:02 +0100

    Made a web scraper

---
+++ Python/Web_Scraper/web_scraper.py
@@ -0,0 +1,24 @@
+import scrapy
+
+class MySpider(scrapy.Spider):
+
+    name = 'myspider'
+    start_urls = ['https://example.com']
+
+    def parse(self, response):
+
+        # Extracting data from the response using XPath or CSS selectors
+        data = response.xpath('//h2[@class="article-title"]/text()').getall()
+
+        # Print the extracted data
+        for item in data:
+            
+            print(item)
+
+# Running the spider
+if __name__ == "__main__":
+
+    from scrapy.crawler import CrawlerProcess
+    process = CrawlerProcess()
+    process.crawl(MySpider)
+    process.start()
